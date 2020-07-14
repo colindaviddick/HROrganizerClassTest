a604681a2f162bdb6c0c35ee40691ab7fd3ae339
Colin Dick <colindaviddick@outlook.com>
2020-07-14 23:36:48 +0100

    Add project files.

---
+++ HROrganizerClassTest/Employee.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace HROrganizerClassTest
+{
+    public class Employee
+    {
+        string Name { get; set; }
+        string JobTitle { get; set; }
+        string BirthMonth { get; set; }
+        int StartYear { get; set; }
+        int Salary { get; set; }
+        bool IsOnHoliday { get; set; }
+
+        public Employee(string name)
+        {
+            Name = name;
+            JobTitle = "Not set";
+            BirthMonth = "Not set";
+            StartYear = 2020;
+            Salary = 15000;
+            IsOnHoliday = false;
+        }
+        public Employee(string name, string jobTitle)
+        {
+            Name = name;
+            JobTitle = jobTitle;
+            BirthMonth = "Not set";
+            StartYear = 2020;
+            Salary = 15000;
+            IsOnHoliday = false;
+        }
+        public Employee(string name, string jobTitle, int startYear, int salary)
+        {
+            Name = name;
+            JobTitle = jobTitle;
+            BirthMonth = "Not set";
+            StartYear = startYear;
+            Salary = salary;
+            IsOnHoliday = false;
+        }
+
+        public Employee(string name, string jobTitle, string birthMonth, int startYear, int salary, bool isOnHoliday)
+        {
+            Name = name;
+            JobTitle = jobTitle;
+            BirthMonth = birthMonth;
+            StartYear = startYear;
+            Salary = salary;
+            IsOnHoliday = isOnHoliday;
+        }
+
+        public Employee()
+        {
+
+        }
+
+        public class Manager : Employee
+        {
+            bool HasCompanyCar { get; set; }
+            int Bonus { get; set; }
+
+            public Manager(string name, string jobTitle, string birthMonth, int startYear, int salary, bool isOnHoliday, bool hasCompanyCar, int bonus)
+            {
+                Name = name;
+                JobTitle = jobTitle;
+                BirthMonth = birthMonth;
+                StartYear = startYear;
+                Salary = salary;
+                IsOnHoliday = isOnHoliday;
+                HasCompanyCar = hasCompanyCar;
+                Bonus = bonus;
+            }
+            public override void DisplayEmployee()
+            {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("_______ _ _ _  _   _     _");
+                Console.WriteLine("|    MANAGER");
+                Console.WriteLine("|");
+                Console.WriteLine("|  - Name            : " + Name);
+                Console.WriteLine("|  - Job Title       : " + JobTitle);
+                Console.WriteLine("|  - Birth Month     : " + BirthMonth);
+                Console.WriteLine("|  - Start Year      : " + StartYear);
+                Console.WriteLine("|  - Salary          : " + Salary);
+                if (IsOnHoliday)
+                {
+                    Console.Write("|  - Is On Holiday?  : ");
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(IsOnHoliday);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                }
+                else
+                {
+                    Console.WriteLine("|  - Is On Holiday?  : " + IsOnHoliday);
+                }
+                Console.WriteLine("|  - Has Company Car : " + HasCompanyCar);
+                Console.WriteLine("|  - Bonus           : " + Bonus);
+                Console.WriteLine("|_________ _ _ _  _   _     _");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+        }
+
+        public virtual void DisplayEmployee()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("_______ _ _ _  _   _     _");
+            Console.WriteLine("|");
+            Console.WriteLine("|  - Name            : " + Name);
+            Console.WriteLine("|  - Job Title       : " + JobTitle);
+            Console.WriteLine("|  - Birth Month     : " + BirthMonth);
+            Console.WriteLine("|  - Start Year      : " + StartYear);
+            Console.WriteLine("|  - Salary          : " + Salary);
+            if(IsOnHoliday)
+            {
+                Console.Write("|  - Is On Holiday?  : ");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(IsOnHoliday);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            else
+            {
+                Console.WriteLine("|  - Is On Holiday?  : " + IsOnHoliday);
+            }
+            Console.WriteLine("|_________ _ _ _  _   _     _");
+        }
+    }
+}
---
+++ HROrganizerClassTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HROrganizerClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee colin =    new Employee("Colin Dick", "Animal Technician", "November", 2012, 21000, false);
+            Employee samantha = new Employee("Samantha Clabby", "Baker", "October", 2008, 12000, false);
+            Employee ayla =     new Employee.Manager("Ayla Guler", "Manageress", "August", 2016, 35000, false, true, 5000);
+            Employee victoria = new Employee("Victoria Clabby");
+            Employee david =    new Employee("David Dick", "Brewery Worker", 1980, 26000);
+            Employee muriel =   new Employee("Muriel Dick", "Saleswoman");
+            Employee elma =     new Employee.Manager("Elma Guler", "Deputy Manager", "September", 2018, 23000, true, true, 10000);
+            Employee jamie =    new Employee("Jamie Clabby", "Teacher", 2000, 24000);
+            Employee jacqui =   new Employee("Jacqui Clabby", "Archaeologist", "October", 2000, 30000, true);
+            Employee jennifer = new Employee("Jennifer Guler", "Child Minder");
+            Employee gunay =    new Employee("Gunay Guler", "Barber", 2016, 19000);
+
+            employees.Add(colin);
+            employees.Add(samantha);
+            employees.Add(ayla);
+            employees.Add(elma);
+            employees.Add(victoria);
+            employees.Add(david);
+            employees.Add(muriel);
+            employees.Add(jamie);
+            employees.Add(jennifer);
+            employees.Add(gunay);
+            employees.Add(jacqui);
+
+            foreach(Employee e in employees)
+            {
+                e.DisplayEmployee();
+            }
+            Console.Read();
+        }
+    }
+}
